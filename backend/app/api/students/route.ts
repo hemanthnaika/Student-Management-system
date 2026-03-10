b5fc068ee839d18750ebe4c45a72db34401d38cc
hemanthnaika <hemanthnaika12@gmail.com>
2026-03-10 14:10:54 +0530

    update origin

--- backend/app/api/students/route.ts
+++ backend/app/api/students/route.ts
@@ -5,7 +5,7 @@ export async function OPTIONS() {
   return new Response(null, {
     status: 200,
     headers: {
-      "Access-Control-Allow-Origin": "http://localhost:5173",
+      "Access-Control-Allow-Origin": process.env.FRONTEND_URL!,
       "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
       "Access-Control-Allow-Headers": "Content-Type, Authorization",
     },
